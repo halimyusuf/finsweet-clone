a252263647afb26910aa0bafee3de95ba1dced29
halimyusuf <haleemyoosuph@gmail.com>
2022-03-15 00:20:27 +0100

    chore: add postimg to allowed domains

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: false, // it should be false by default
   images: {
-    domains: ["www.pexels.com"],
+    domains: ["i.postimg.cc"],
   },
 };
