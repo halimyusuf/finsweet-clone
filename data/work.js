dc35e30d15a47bd6ec9f07d894bc9d4979b28d60
halimyusuf <haleemyoosuph@gmail.com>
2022-03-20 14:20:32 +0100

    feat: added work page

---
+++ data/work.js
@@ -0,0 +1,44 @@
+export const workExamples = [
+  {
+    bg: "linear-gradient(131.34deg, #F4EDE1 16.74%, #98B1C6 103.17%)",
+    imgUrl: "https://i.postimg.cc/k4wcn99D/image-15.jpg",
+    title: "Template 1",
+    subtitle:
+      "Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.",
+  },
+  {
+    bg: "linear-gradient(131.34deg, #FFD3AF 16.74%, #4F56FF 103.17%)",
+    imgUrl: "https://i.postimg.cc/HWZ2RpBh/image-15-2.jpg",
+    title: "Template 2",
+    subtitle:
+      "Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.",
+  },
+  {
+    bg: "linear-gradient(131.34deg, #EEBCA7 16.74%, #C8C8C8 103.17%)",
+    imgUrl: "https://i.postimg.cc/brJRrMNg/image-15-3.jpg",
+    title: "Template 3",
+    subtitle:
+      "Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.",
+  },
+  {
+    bg: "linear-gradient(131.34deg, #C2E7F2 16.74%, #98A4FF 103.17%)",
+    imgUrl: "https://i.postimg.cc/50fgdyhZ/image-15-4.jpg",
+    title: "Template 4",
+    subtitle:
+      "Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.",
+  },
+  {
+    bg: "linear-gradient(131.34deg, #C5BCFF 16.74%, #9BF7D5 103.17%)",
+    imgUrl: "https://i.postimg.cc/s2YmN7NY/image-15-5.jpg",
+    title: "Template 5",
+    subtitle:
+      "Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.",
+  },
+  {
+    bg: "linear-gradient(131.34deg, #D2D6FD 16.74%, #D6ACEA 103.17%)",
+    imgUrl: "https://i.postimg.cc/QCyJTbF3/image-15-6.jpg",
+    title: "Template 6",
+    subtitle:
+      "Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.",
+  },
+];
---
+++ pages/work/index.js
@@ -0,0 +1,177 @@
+import { Button, Typography } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import ArrowRight from "../../components/icons/ArrowRight";
+import { workExamples } from "../../data/work";
+
+export default function Work() {
+  return (
+    <div>
+      <div className="bg-grey">
+        <div className="container mx-auto py-14">
+          <div className="text-center">
+            <div>
+              <Typography variant="subtitle2" sx={{ color: "var(--darkblue)" }}>
+                What we created
+              </Typography>
+            </div>
+            <div className="my-2">
+              <Typography
+                variant="h4"
+                sx={{
+                  fontWeight: "600",
+                  color: "var(--darkblue)",
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                Our Work Portfolio
+              </Typography>
+              <Typography
+                variant="h3"
+                sx={{
+                  fontWeight: "600",
+                  color: "var(--darkblue)",
+                  display: { xs: "none", md: "block", lg: "none" },
+                }}
+              >
+                Our Work Portfolio
+              </Typography>
+              <Typography
+                variant="h2"
+                sx={{
+                  fontWeight: "600",
+                  color: "var(--darkblue)",
+                  display: { xs: "none", lg: "block" },
+                }}
+              >
+                Our Work Portfolio
+              </Typography>
+            </div>
+
+            <div
+              className="w-max-90%"
+              style={{
+                maxWidth: "90%",
+                width: "700px",
+                textAlign: "center",
+                margin: "0 auto",
+              }}
+            >
+              <Typography
+                variant="body2"
+                sx={{ color: "var(--darkblue)", opacity: "0.7" }}
+              >
+                We help teams create great digital products by providing them
+                with tools and technology to make the design-to-code process
+                universally accessible.
+              </Typography>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="container mx-auto my-14 text-darkblue">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            {workExamples.map((work) => (
+              <div key={work.title} className="my-2 px-2">
+                <div>
+                  <div style={{ background: work.bg }}>
+                    <div className="mx-4 py-4">
+                      <div className="aspect-video w-[100%] mx-auto relative">
+                        <Image
+                          src={work.imgUrl}
+                          layout="fill"
+                          objectFit="cover"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <Typography variant="h6" sx={{ fontWeight: 600 }}>
+                    {work.title}
+                  </Typography>
+                </div>
+                <div className="my-1">
+                  <Typography variant="subtitle1" sx={{ opacity: 0.7 }}>
+                    {work.subtitle}
+                  </Typography>
+                </div>
+                <div className="flex items-center mt-2">
+                  <Link href={"/"} passHref>
+                    <Typography
+                      variant="subtitle2"
+                      className="mr-3 cursor-pointer hover:underline text-darkblue"
+                    >
+                      Read More
+                    </Typography>
+                  </Link>
+                  <div>
+                    <ArrowRight />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="container mx-auto my-14">
+          <div className="text-center mb-2">
+            <Typography
+              variant="h4"
+              sx={{ fontWeight: 600, display: { xs: "none", md: "block" } }}
+            >
+              Let's build something great together
+            </Typography>
+
+            <Typography
+              variant="h5"
+              sx={{ fontWeight: 600, display: { xs: "block", md: "none" } }}
+            >
+              Let's build something <br></br> great together
+            </Typography>
+          </div>
+
+          <div className="py-2">
+            <div
+              className="w-max-90%"
+              style={{
+                maxWidth: "90%",
+                width: "550px",
+                textAlign: "center",
+                margin: "0 auto",
+              }}
+            >
+              <Typography
+                variant="body2"
+                sx={{ color: "var(--darkblue)", opacity: "0.7" }}
+              >
+                Nullam vitae purus at tortor mattis dapibus. Morbi purus est,
+                ultricies nec dolor sit amet, scelerisque cursus purus.
+              </Typography>
+            </div>
+          </div>
+
+          <div className="text-center mt-4">
+            <Button
+              size="large"
+              sx={{
+                background: "var(--yellow) !important",
+                color: "black",
+                borderRadius: "999px",
+                padding: "0.5rem 2.5rem",
+                "&:hover": { background: "var(--lightyellow)" },
+              }}
+            >
+              <Typography variant="body-2 capitalize">Contact Us</Typography>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
