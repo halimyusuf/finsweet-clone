2ab7aa260e181d229a1189b84586da43b851e6f8
halimyusuf <haleemyoosuph@gmail.com>
2022-03-23 22:49:28 +0100

    feat: added nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import "../styles/globals.css";
 import Layout from "../components/layout";
+
+import Router from "next/router";
+import NProgress from "nprogress"; //nprogress module
+import "nprogress/nprogress.css"; //styles of nprogress
+
+import "../styles/globals.css";
+//Binding events.
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
